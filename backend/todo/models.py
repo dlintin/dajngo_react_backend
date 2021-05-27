2b0a10cd05e7ecadfda9e685d94a6c4fcf07a5d0
dlintin <dev.dianto.lintin@gmail.com>
2021-05-27 17:40:13 +0800

    Initial commit

---
+++ backend/todo/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+# Create your models here.
+
+# class Todo(models.Model):
+#     title = models.CharField(max_length=120)
+#     description = models.TextField()
+#     completed = models.BooleanField(default=False)
+# 	image = models.ImageField()
+#
+#     def _str_(self):
+#         return self.title
